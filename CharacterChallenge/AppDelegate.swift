68f4ce0efd876fdee8cfe15f39f745286343cbdc
ramy nasser <ramynasser95@gmail.com>
2024-09-11 17:26:54 +0300

    add appdelegate

--- CharacterChallenge/AppDelegate.swift
+++ CharacterChallenge/AppDelegate.swift
@@ -6,31 +6,35 @@
 //
 
 import UIKit
-
+import Logger
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    static var shared: AppDelegate!
+    
+    var appConfig: AppConfig = {
+        guard let appConfig = AppConfig() else {
+            let errorMessage = "Invalid app config"
+            LogsManager.shared.critical(message: errorMessage, error: nil)
+            fatalError(errorMessage)
+        }
+        
+        return appConfig
+    }()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Self.shared = self
+        setup()
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
+    
+    func setup() {
+         _ = [
+             DataConfigurator.shared,
+             LoggerConfigurator.shared,
+             DesignSystemConfigurator.shared
+         ].map { $0.setup() }
+     }
+    
 }
 
