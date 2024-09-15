02ac621abfddc1a969c4eb4665c50d90042e1265
ramy nasser <ramynasser95@gmail.com>
2024-09-15 14:31:23 +0300

    enhance code quality

--- CharacterChallenge/App/Scenes/Listing/View/CharactersListViewController.swift
+++ CharacterChallenge/App/Scenes/Listing/View/CharactersListViewController.swift
@@ -12,7 +12,7 @@ import SwiftUI
 import Core
 
 class CharactersListViewController: UIViewController {
-
+    //MARK: - Outlet Variables
     @IBOutlet weak var filterCollectionTop: NSLayoutConstraint!
     @IBOutlet weak var filterCollectionLeading: NSLayoutConstraint!
     @IBOutlet weak var filterCollectionTrailing: NSLayoutConstraint!
@@ -20,7 +20,7 @@ class CharactersListViewController: UIViewController {
     @IBOutlet weak var tableViewBottom: NSLayoutConstraint!
     @IBOutlet weak var charactersTableView: UITableView!
     @IBOutlet weak var filterCollectionView: UICollectionView!
-
+    //MARK: - Private Variables
     private var loadingDataSource: LoadingDataSource?
     private var populateDataSource: PopulateDataSource?
     private var filterDataSource: FilterDataSource?
@@ -28,17 +28,17 @@ class CharactersListViewController: UIViewController {
     private var emptyDataSource: EmptyDataSource?
     private let viewModel: CharactersListViewModel
     private var cancellables = Set<AnyCancellable>()
-
+    
     //MARK: - Init
     init(viewModel: CharactersListViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel.load()
@@ -60,21 +60,8 @@ class CharactersListViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "Characters"
     }
-    private func navigate(index: Int) {
-        struct UseCase: CharacterDetailCoordinatorUseCaseProtocol {
-            var characterId: Int
-            var navigationController: UINavigationController?
-        }
-
-        coordinator = CharacterDetailCoordinator(
-            useCase: UseCase(
-                characterId: viewModel.characters[index].id,
-                navigationController: self.navigationController
-            )
-        )
-        coordinator?.start()
-    }
-
+    //MARK: - Private Method
+    /// Sets up the observers for handling state changes in the ViewModel.
     private func viewModelObservers() {
         viewModel.$state.sink { [weak self] state in
             guard let self = self else {
@@ -83,27 +70,10 @@ class CharactersListViewController: UIViewController {
             switch state {
             case .loading:
                 self.loadingDataSource = LoadingDataSource(tableView: self.charactersTableView)
-
+                
             case let .loaded(items):
-                self.populateDataSource = PopulateDataSource(
-                    tableView: self.charactersTableView,
-                    characters: items,
-                    callback: { isPaging in
-                        if isPaging {
-                            self.viewModel.didFinishScroll()
-                        }
-                    },
-                    selectionCallBack: { index in
-                        self.navigate(index: index)
-                    })
-                self.filterDataSource = FilterDataSource(
-                    collectionView: self.filterCollectionView,
-                    filters: viewModel.filters,
-                    selectedFilter: viewModel.selectedFilter,
-                    callback: { filter in
-                        self.viewModel.selectedFilter = filter
-                    }
-                )
+                self.setupPopulateDataSource(items: items)
+                self.setupFilterDataSource()
             case .empty:
                 emptyDataSource = EmptyDataSource(tableView: self.charactersTableView)
             default:
@@ -111,5 +81,44 @@ class CharactersListViewController: UIViewController {
             }
         }
         .store(in: &cancellables)
+    }
+    private func navigate(index: Int) {
+        struct UseCase: CharacterDetailCoordinatorUseCaseProtocol {
+            var characterId: Int
+            var navigationController: UINavigationController?
+        }
+        
+        coordinator = CharacterDetailCoordinator(
+            useCase: UseCase(
+                characterId: viewModel.characters[index].id,
+                navigationController: self.navigationController
+            )
+        )
+        coordinator?.start()
+    }
+    /// Sets up filter data source
+    private func setupFilterDataSource() {
+        filterDataSource = FilterDataSource(
+            collectionView: filterCollectionView,
+            filters: viewModel.filters,
+            selectedFilter: viewModel.selectedFilter,
+            callback: { filter in
+                self.viewModel.selectedFilter = filter
+            }
+        )
+    }
+    /// Sets up populate data source
+    private func setupPopulateDataSource(items: [CharacterModel]) {
+        self.populateDataSource = PopulateDataSource(
+            tableView: self.charactersTableView,
+            characters: items,
+            callback: { isPaging in
+                if isPaging {
+                    self.viewModel.didFinishScroll()
+                }
+            },
+            selectionCallBack: { index in
+                self.navigate(index: index)
+            })
     }
 }
